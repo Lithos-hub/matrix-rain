4fa63e462a06db08adafc7b81e9aee998e2f4b06
Carlos Segura Garcia <lithos.contact@gmail.com>
2024-04-04 13:32:07 +0200

    remove version, add github button

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import "./style.css";
 import { init } from "./app.ts";
-import { version } from "../package.json";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-<canvas id="canvas" width="800" height="600"></canvas>
-<div class="transparent-box version">v${version}</div>
-<button id="btn__fullscreen" class="transparent-box fullscreen"><img src="/public/expand.svg" width="17" height="17" onclick="triggerFullScreen()" /></button>
+    <canvas id="canvas" width="800" height="600"></canvas>
+
+    <div class="buttons">
+        <a href="https://github.com/Lithos-hub/matrix-rain" target="_blank">
+            <button id="btn__github" class="transparent-box">
+                <img src="/public/icons/github.svg" width="17" height="17" />
+            </button>
+        </a>
+        <button id="btn__fullscreen" class="transparent-box">
+            <img src="/public/icons/expand.svg" width="17" height="17" />
+        </button>
+    </div>
 `;
 
 init();
